6184f0ee2b5be8697350b6d059067bf10264705a
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-10-29 15:38:11 +0900

    01

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int score[4];
+	score[0]=60;
+	score[1]=70;
+	score[2]=80;
+	score[3]=90;
+	score[4]=100;
+	
+	return 0;
+}
