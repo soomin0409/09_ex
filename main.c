1a8496264f6e252d79764456f3bdace3cc8f0361
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-10-29 15:39:49 +0900

    01-2

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int score[4];
+	int score[5];
 	score[0]=60;
 	score[1]=70;
 	score[2]=80;
