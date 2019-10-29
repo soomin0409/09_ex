d5b34f353ef632848e3d0fa0a06a5c072793f5ca
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-10-29 15:44:40 +0900

    01-3

--- main.c
+++ main.c
@@ -4,12 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int score[5];
+	int score[5],i;
 	score[0]=60;
 	score[1]=70;
 	score[2]=80;
 	score[3]=90;
 	score[4]=100;
+	for(i=0;i<5;i++)
+	{
+		printf("score[%d]=%d\n",i,score[i]);
+	}
 	
 	return 0;
 }
